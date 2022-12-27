34e29552e84de91da331ca9fe2825c9a33329de5
HajagosNorbert <hajagosnorbi@gmail.com>
2022-12-27 13:51:29 +0100

    No additional checks for .nvmrc files on cd. More autostarting programs for qtile.

--- .config/fish/functions/nvm_find_nvmrc.fish
+++
@@ -1,4 +0,0 @@
-function nvm_find_nvmrc
-  bass source /usr/share/nvm/nvm.sh --no-use ';' nvm_find_nvmrc
-end
-
--- .config/qtile/modules/keys.py
+++ .config/qtile/modules/keys.py
@@ -50,6 +50,7 @@ keys = [
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
+    Key([mod], "f", lazy.window.toggle_floating(), desc='Toggle floating'),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
