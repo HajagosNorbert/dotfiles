9cb53ccbb0af8c6b33ffd02d21367f09dfcd2398
HajagosNorbert <hajagosnorbi@gmail.com>
2023-05-14 16:54:30 +0200

    rust-tools works. Now trying to separate it from lspconfig

--- .config/nvim/lua/ez/plugin/lsp.lua
+++ .config/nvim/lua/ez/plugin/lsp.lua
@@ -8,7 +8,22 @@ return {
             -- Note: autocompletion settings will not take effect
 
             require('lsp-zero.settings').preset({
-                name = 'recommended',
+                float_border = 'rounded',
+                call_servers = 'local',
+                configure_diagnostics = true,
+                setup_servers_on_start = true,
+                set_lsp_keymaps = {
+                    preserve_mappings = false,
+                    omit = {},
+                },
+                manage_nvim_cmp = {
+                    set_sources = 'recommended',
+                    set_basic_mappings = true,
+                    set_extra_mappings = false,
+                    use_luasnip = true,
+                    set_format = true,
+                    documentation_window = true,
+                },
             })
         end
     },
@@ -61,13 +76,12 @@ return {
                     pcall(vim.cmd, 'MasonUpdate')
                 end,
             },
+            { 'simrat39/rust-tools.nvim' }
         },
         config = function()
-            -- This is where all the LSP shenanigans will live
-
             local lsp = require('lsp-zero')
 
-            lsp.on_attach(function(client, bufnr)
+            lsp.on_attach(function(_, bufnr)
                 lsp.default_keymaps({ buffer = bufnr })
 
                 vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename)
@@ -79,8 +93,18 @@ return {
 
             -- (Optional) Configure lua language server for neovim
             require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
+            lsp.skip_server_setup({ 'rust_analyzer' })
             lsp.setup()
+
+            local rust_tools = require('rust-tools')
+
+            rust_tools.setup({
+                server = {
+                    on_attach = function()
+                        vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+                    end
+                }
+            })
         end
     },
     {
