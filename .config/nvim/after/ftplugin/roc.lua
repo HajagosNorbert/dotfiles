ee716b5972fa191b139a9a48bb7c26e7ecec3a64
HajagosNorbert <hajagosnorbi@gmail.com>
2024-01-14 13:44:43 +0100

    no after folder needed

--- .config/nvim/after/ftplugin/roc.lua
+++
@@ -1,7 +0,0 @@
-  -- set syntax=elm
-vim.opt.syntax = 'elm'
--- vim.lsp.start({
---     name = "roc_ls",
---     cmd = { vim.fn.expand("$HOME/code/roc/target/release/roc_ls") },
---     root_dir = vim.fn.getcwd(),
--- })
